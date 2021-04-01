edebf11aaaaf3564a7e4d860f7263c2bee22b1ea
srusinchuk <srusinchuk@b2bsoft.com>
2021-04-01 21:12:49 +0300

    Homework Lesson_3_task_4 done. @elpiankova

---
+++ Lesson_3_task_4.py
@@ -0,0 +1,19 @@
+l = [65, 78, 954, 43, 423, 34]
+while l:
+    l.pop()
+    print(l)
+
+z ='qwerty123'
+while z:
+    z = z [1:]
+    z = l
+    print(z)
+
+k = [65, 31, 98, 35, 65, 12, 10]
+k.sort()
+k.reverse()
+print(k)
+while k:
+    k.pop()
+    print(k)
+
